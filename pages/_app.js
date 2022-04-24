ad451141cc210f7041c4950647e62c8d8b1c9151
Jorge <jhessle04@gmail.com>
2022-04-24 11:14:38 -0300

    N

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/globals.css'
 import { TransactionProvider } from '../context/TransactionContext'
+import React, { Component } from 'react'
 
 function MyApp({ Component, pageProps }) {
   return (
